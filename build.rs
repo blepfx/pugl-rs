ecef56d3ae69273bdda40eea5c925c5716eded97
Quant1um <spitfirexv22@gmail.com>
2025-02-22 15:08:25 +0400

    switch from calling meson to using cc (x11 tested only for now)

--- build.rs
+++ build.rs
@@ -1,138 +1,81 @@
-use std::process::Command;
-
 fn main() {
     let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-    let profile = std::env::var("PROFILE").unwrap();
 
     println!("cargo:rerun-if-changed=pugl/meson.build");
 
-    Command::new("meson")
-        .current_dir("pugl")
-        .args([
-            "setup",
-            "build",
-            "--reconfigure",
-            "--buildtype",
-            if profile == "release" {
-                "release"
-            } else {
-                "debug"
-            },
-            &format!(
-                "-Dopengl={}",
-                if cfg!(feature = "opengl") {
-                    "enabled"
-                } else {
-                    "disabled"
-                }
-            ),
-            &format!(
-                "-Dvulkan={}",
-                if cfg!(feature = "vulkan") {
-                    "enabled"
-                } else {
-                    "disabled"
-                }
-            ),
-            &format!(
-                "-Dcairo={}",
-                if cfg!(feature = "cairo") {
-                    "enabled"
-                } else {
-                    "disabled"
-                }
-            ),
-            "-Dexamples=disabled",
-            "-Dtests=disabled",
-            "-Dbindings_cpp=disabled",
-            "-Ddefault_library=static",
-        ])
-        .status()
-        .expect("pugl: meson build failed");
-
-    Command::new("meson")
-        .arg("compile")
-        .current_dir("pugl/build")
-        .status()
-        .expect("pugl: meson compile failed");
-
-    let mut libs = vec![];
-
     if os == "linux" {
-        libs.extend(["X11", "Xext", "Xrandr", "Xcursor"]);
-
-        if cfg!(feature = "opengl") {
-            libs.push("GL");
-        }
-
-        if cfg!(feature = "cairo") {
-            libs.push("cairo");
-        }
-
-        if cfg!(feature = "vulkan") {
-            libs.push("vulkan");
-        }
+        build_linux();
     } else if os == "windows" {
-        libs.extend(["shlwapi", "dwmapi", "user32", "gdi32"]);
-
-        if cfg!(feature = "cairo") {
-            libs.push("cairo");
-        }
-
-        if cfg!(feature = "vulkan") {
-            libs.push("vulkan");
-        }
+        build_windows();
     } else if os == "macos" {
+        build_macos();
     } else {
-        panic!("unsupported target os: only linux, windows and macos are supported")
-    };
+        panic!("unsupported platform: {}", os);
+    }
 
-    if os == "linux" {
-        println!("cargo:rustc-link-search=native=./pugl/build");
-        println!("cargo:rustc-link-lib=static=pugl_x11-0");
-        println!("cargo:rustc-link-lib=static=pugl_x11_stub-0");
+    // if os == "linux" {
+    // } else if os == "windows" {
+    //     println!("cargo:rustc-link-search=native=./pugl/build");
+    //     println!("cargo:rustc-link-lib=static=pugl_win-0");
+    //     println!("cargo:rustc-link-lib=static=pugl_win_stub-0");
 
-        if cfg!(feature = "cairo") {
-            println!("cargo:rustc-link-lib=static=pugl_x11_cairo-0");
-        }
-        if cfg!(feature = "opengl") {
-            println!("cargo:rustc-link-lib=static=pugl_x11_gl-0");
-        }
-        if cfg!(feature = "vulkan") {
-            println!("cargo:rustc-link-lib=static=pugl_x11_vulkan-0");
-        }
-    } else if os == "windows" {
-        println!("cargo:rustc-link-search=native=./pugl/build");
-        println!("cargo:rustc-link-lib=static=pugl_win-0");
-        println!("cargo:rustc-link-lib=static=pugl_win_stub-0");
+    //     if cfg!(feature = "cairo") {
+    //         println!("cargo:rustc-link-lib=static=pugl_win_cairo-0");
+    //     }
+    //     if cfg!(feature = "opengl") {
+    //         println!("cargo:rustc-link-lib=static=pugl_win_gl-0");
+    //     }
+    //     if cfg!(feature = "vulkan") {
+    //         println!("cargo:rustc-link-lib=static=pugl_win_vulkan-0");
+    //     }
+    // } else if os == "macos" {
+    //     println!("cargo:rustc-link-search=native=./pugl/build");
+    //     println!("cargo:rustc-link-lib=static=pugl_mac-0");
+    //     println!("cargo:rustc-link-lib=static=pugl_mac_stub-0");
 
-        if cfg!(feature = "cairo") {
-            println!("cargo:rustc-link-lib=static=pugl_win_cairo-0");
-        }
-        if cfg!(feature = "opengl") {
-            println!("cargo:rustc-link-lib=static=pugl_win_gl-0");
-        }
-        if cfg!(feature = "vulkan") {
-            println!("cargo:rustc-link-lib=static=pugl_win_vulkan-0");
-        }
-    } else if os == "macos" {
-        println!("cargo:rustc-link-search=native=./pugl/build");
-        println!("cargo:rustc-link-lib=static=pugl_mac-0");
-        println!("cargo:rustc-link-lib=static=pugl_mac_stub-0");
+    //     if cfg!(feature = "cairo") {
+    //         println!("cargo:rustc-link-lib=static=pugl_mac_cairo-0");
+    //     }
+    //     if cfg!(feature = "opengl") {
+    //         println!("cargo:rustc-link-lib=static=pugl_mac_gl-0");
+    //
+    //     }
+    //     if cfg!(feature = "vulkan") {
+    //         println!("cargo:rustc-link-lib=static=pugl_mac_vulkan-0");
+    //     }
+    // }
+}
 
-        println!("cargo:rustc-link-lib=framework=AppKit");
-        println!("cargo:rustc-link-lib=framework=CoreVideo");
+fn build_linux() {
+    let mut build = cc::Build::new();
 
-        if cfg!(feature = "cairo") {
-            println!("cargo:rustc-link-lib=static=pugl_mac_cairo-0");
-        }
-        if cfg!(feature = "opengl") {
-            println!("cargo:rustc-link-lib=static=pugl_mac_gl-0");
-            println!("cargo:rustc-link-lib=framework=OpenGL");
-        }
-        if cfg!(feature = "vulkan") {
-            println!("cargo:rustc-link-lib=static=pugl_mac_vulkan-0");
-        }
+    build.include("pugl/include");
+    build.file("pugl/src/common.c");
+    build.file("pugl/src/internal.c");
+    build.file("pugl/src/x11.c");
+    build.file("pugl/src/x11_stub.c");
+
+    if cfg!(feature = "cairo") {
+        build.file("pugl/src/x11_cairo.c");
+    }
+    if cfg!(feature = "opengl") {
+        build.file("pugl/src/x11_gl.c");
+    }
+    if cfg!(feature = "vulkan") {
+        build.file("pugl/src/x11_vulkan.c");
+    }
+
+    build.compile("pugl_x11");
+
+    let mut libs = vec!["X11", "Xext", "Xrandr", "Xcursor"];
+    if cfg!(feature = "cairo") {
+        libs.push("cairo");
+    }
+    if cfg!(feature = "opengl") {
+        libs.push("GL");
+    }
+    if cfg!(feature = "vulkan") {
+        libs.push("vulkan");
     }
 
     println!(
@@ -142,4 +85,59 @@ fn main() {
             .collect::<Vec<_>>()
             .join(" ")
     );
+}
+
+fn build_windows() {
+    let mut build = cc::Build::new();
+
+    build.include("pugl/include");
+    build.file("pugl/src/common.c");
+    build.file("pugl/src/internal.c");
+    build.file("pugl/src/win.c");
+    build.file("pugl/src/win_stub.c");
+
+    if cfg!(feature = "cairo") {
+        build.file("pugl/src/win_cairo.c");
+    }
+    if cfg!(feature = "opengl") {
+        build.file("pugl/src/win_gl.c");
+    }
+    if cfg!(feature = "vulkan") {
+        build.file("pugl/src/win_vulkan.c");
+    }
+
+    build.compile("pugl_win");
+
+    //TODO: link to the required libraries
+}
+
+fn build_macos() {
+    let mut build = cc::Build::new();
+
+    build.include("pugl/include");
+    build.file("pugl/src/common.c");
+    build.file("pugl/src/internal.c");
+    build.file("pugl/src/mac.c");
+    build.file("pugl/src/mac_stub.c");
+
+    if cfg!(feature = "cairo") {
+        build.file("pugl/src/mac_cairo.c");
+    }
+    if cfg!(feature = "opengl") {
+        build.file("pugl/src/mac_gl.c");
+    }
+    if cfg!(feature = "vulkan") {
+        build.file("pugl/src/mac_vulkan.c");
+    }
+
+    build.compile("pugl_mac");
+
+    println!("cargo:rustc-link-lib=framework=AppKit");
+    println!("cargo:rustc-link-lib=framework=CoreVideo");
+
+    if cfg!(feature = "opengl") {
+        println!("cargo:rustc-link-lib=framework=OpenGL");
+    }
+
+    //TODO: link to the required frameworks
 }
